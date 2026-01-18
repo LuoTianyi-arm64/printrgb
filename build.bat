f6220780046c85c01317d835830c6eab7584134c
LuoTainyi-arm64 <2153585992@qq.com>
2026-01-18 18:27:31 +0800

    v1.1.5

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
-pip uninstall
-printrgb -y
-set "ver=1.1.4"
+pip uninstall printrgb -y
+set "ver=1.1.5"
 py -m build --no-isolation
 pip install .\dist\printrgb-%ver%-py3-none-any.whl
--- src/printrgb/__main__.py
+++ src/printrgb/__main__.py
@@ -2,7 +2,7 @@
 import sys
 from . import printrgb
 
-version = "1.1.4"
+version = "1.1.5"
 
 def main() -> None:
     argv = sys.argv
